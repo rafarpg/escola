70f5574d074c891b5d85fb33e81a3707eedee235
rafarpg <rafaelpassosgol@gmail.com>
2023-05-02 09:09:51 -0300

    escola(2)

--- crud.php
+++ crud.php
@@ -8,25 +8,35 @@
 if(isset($_POST["inserir"]))
 { 
     $comando = $pdo->prepare("INSERT INTO alunos(nome,idade) VALUES('$nome', $idade)");
+    $resultado = $comando->execute();  
+    header("Location: cadastro.html");
 }
 
 if(isset($_POST["excluir"]))
 
 {
     $comando = $pdo->prepare("DELETE FROM alunos WHERE matricula=$matricula");
+    $resultado = $comando->execute();  
+    header("Location: cadastro.html");
 }
 
 if(isset($_POST["alterar"]) )
 {
     $comando = $pdo->prepare("UPDATE alunos SET nome='$nome', idade=$idade WHERE matricula=$matricula");
+    $resultado = $comando->execute();  
+    header("Location: cadastro.html");
 }
-
-if(isset($_POST["lsitar"]) )
+if(isset($_POST["listar"]))
 {
-    $comando = $pdo->prepare("SELECT * FROM alunos WHERE matricula=$matricula");
-}
+    $comando = $pdo->prepare("SELECT * FROM alunos WHERE matricula=$matricula ");
+    $resultados = $comando->execute();
 
-$resultados = $comando->execute();
-    
-header("Location: cadastro.html");
+    while($linhas = $comando->fetch())
+    {
+        $m = $linhas["matricula"];
+        $n = $linhas["nome"];
+        $i = $linhas["idade"];
+        echo(" Nome: $n  idade: $i matricula: $m <br> <br>");
+    }
+} 
 ?>
