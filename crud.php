34c98d1068c877e43ea33ef15a2f5ed2d6f060e8
rafarpg <rafaelpassosgol@gmail.com>
2023-05-02 08:26:34 -0300

    escola

--- crud.php
+++ crud.php
@@ -6,18 +6,27 @@
     $idade = $_POST["idade"];
 
 if(isset($_POST["inserir"]))
-{ $comando = $pdo->prepare("INSERT INTO alunos(nome,idade) VALUES($matricula,'$nome', $idade)");}
+{ 
+    $comando = $pdo->prepare("INSERT INTO alunos(nome,idade) VALUES('$nome', $idade)");
+}
 
 if(isset($_POST["excluir"]))
 
-{$comando = $pdo->prepare("DELETE FROM alunos WHERE matricula=$matricula");}
+{
+    $comando = $pdo->prepare("DELETE FROM alunos WHERE matricula=$matricula");
+}
 
 if(isset($_POST["alterar"]) )
 {
     $comando = $pdo->prepare("UPDATE alunos SET nome='$nome', idade=$idade WHERE matricula=$matricula");
 }
-   
-$resultado = $comando->execute();
 
-    header("Location: cadastro.html");
+if(isset($_POST["lsitar"]) )
+{
+    $comando = $pdo->prepare("SELECT * FROM alunos WHERE matricula=$matricula");
+}
+
+$resultados = $comando->execute();
+    
+header("Location: cadastro.html");
 ?>
